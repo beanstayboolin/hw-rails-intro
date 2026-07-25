153e336fa40034a2a5486c8ba5c56d8d8943fd53
Erick Hernandez Hidalgo <erickh112305@berkeley.edu>
2026-07-25 03:36:58 +0000

    heyoo working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ApplicationRecord
+  def self.all_ratings
+    ["G", "PG", "PG-13", "R"]
+  end
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.present?
+      where("rating IN (?)", ratings_list)
+    else
+      all
+    end
+  end
 end
